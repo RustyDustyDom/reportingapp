d6f9c9954ae54d8abf4c15c7009211be10f6d4ac
Rados Budisa <buda795@gmail.com>
2019-06-03 01:30:41 -0700

    Simple header and footer
    Just fancy stuff

---
+++ templates/inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+        <p>&copy; 2019/2020 <?php echo SITE_TITTLE; ?>, Inc.</p>
+      </footer>
+
+    </div>
+    </body>
+</html>
---
+++ templates/inc/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo SITE_TITTLE; ?></title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><a href="index.php">Home</a></li>
+            <li role="presentation"><a href="create.php">Create Listing</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted"><?php echo SITE_TITTLE; ?></h3>
+      </div>    
+
