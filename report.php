b4eebff5069b3b35c9e86b09e47c40004797e059
Rados Budisa <buda795@gmail.com>
2019-06-03 01:33:10 -0700

    Main template for single records
    This is the template trough which we pass on data and parameters to our single-report.php page.

---
+++ report.php
@@ -0,0 +1,13 @@
+<?php include_once 'config/init.php'; ?>
+
+<?php
+$report = new Reports;
+
+$template = new Template ('templates/report-single.php');
+
+$profile_id = isset($_GET['id']) ? $_GET['id'] : null;
+
+$template->views = $report->getPersonRecord($profile_id);
+$template->records = $report->getPersonMonthly($profile_id);
+
+echo $template;
